316781cc3389e3f9a69ae68d276ab19dcaed3c3f
Sergey Sokolov <dmemory7@ya.ru>
2018-02-20 18:56:45 +0300

    Simple expressions evaluator implementation

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,7 +35,7 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution.
 let _ =
   List.iter
     (fun x ->
@@ -43,12 +43,36 @@ let _ =
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
 
-(* Expression evaluator
+ Expression evaluator
 
      val eval : state -> expr -> int
  
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+
+let eval_binop op l r =
+    let bool_of_int value = if value = 0 then false else true in
+    let int_of_bool value = if value = true then 1 else 0 in
+    match op with
+    | "+" -> l + r
+    | "-" -> l - r
+    | "*" -> l * r
+    | "/" -> l / r
+    | "%" -> l mod r
+    | "==" -> int_of_bool(l == r)
+    | "!=" -> int_of_bool(l != r)
+    | "<"  -> int_of_bool(l < r)
+    | "<=" -> int_of_bool(l <= r)
+    | ">"  -> int_of_bool(l > r)
+    | ">=" -> int_of_bool(l >= r)
+    | "&&" -> int_of_bool(bool_of_int l && bool_of_int r)
+    | "!!" -> int_of_bool(bool_of_int l || bool_of_int r)
+    | _ -> failwith (Printf.sprintf "Unsupported operation: '%s'" op)
+
+let rec eval s expr =
+    let eval' = eval s in
+    match expr with
+    | Const value -> value
+    | Var var -> s var
+    | Binop(op, l, r) -> eval_binop op (eval' l) (eval' r)
